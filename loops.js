a0ab020b5cd91cbc32d3d063babf9f976815902f
Ashley Casey <acasey.dev@gmail.com>
2018-01-09 01:35:30 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,3 @@
-// TODO: Build a function forLoop. It takes an array as an argument. Start counting from 0, and, using a for loop, add a string to the array 25 times. But not just any string. If your i value is 1, add the string "I am 1 strange loop."; if your i value is anything else, add the string "I am ${i} strange loops.". (Remember flow control with if and else? And how do we interpolate i?) Then return the array.
-
-
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if(i == 1) {
@@ -11,4 +8,13 @@ function forLoop(array) {
     }
   }
   return array;
+}
+
+TODO: Create a function called whileLoop in loops.js. The function should take a number as an argument. Using a while loop, count down (using console.log) from the passed in number to 0. Then return the string 'done'.
+
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(--number);
+  }
+  return 'done';
 }
